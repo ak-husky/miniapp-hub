a8de8c301fc73c54b5d7fac1ea383e17998350c2
mofan <mofan@mofan.local>
2024-06-27 07:39:17 +0800

    typo

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -5,11 +5,11 @@ import { useSearchParams } from 'next/navigation';
 export default function Page() {
   const searchParams = useSearchParams();
   const url = searchParams.get('targetUrl');
-  console.log('66666666', url)
+  console.log('target url: ', url)
   return (
     <>
       <main className="flex min-h-screen h-screen">
-        <iframe src={url ?? undefined} width="100%" height="100%" style={{ border: '0' }} />
+        {url && <iframe src={url} width="100%" height="100%" style={{ border: '0' }} />}
       </main>
     </>
   );
